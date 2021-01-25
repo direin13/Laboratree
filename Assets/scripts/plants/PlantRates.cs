9c78dca3ed612861e8ae9f059b8789efe33c627c
Daniel Irein <daniel.irein2@mail.dcu.ie>
2021-01-25 01:36:35 +0000

    updated plant prefab
    Added all attributes of plant (see Dependencies of a plant).

--- Assets/scripts/plants/PlantRates.cs
+++ Assets/scripts/plants/PlantRates.cs
@@ -8,28 +8,39 @@ using MiscFunctions;
 public class PlantRates : MonoBehaviour
 {
     public int SecondsTillDeath; //seconds
+    public GameObject[] allDependencies;
     private int currentSecondsTillDeath;
-
     private readonly float maxEfficiency = 1f;
-    public float lightDependanceRate;
-    public float soilDependanceRate;
-    public float tempDependanceRate;
-    public float wateringDependanceRate;
-    private float[] allRates;
 
 
-    //effiencies represent how much each dependancy is being used
-    public float lightEfficiency;
-    public float soilEfficiency;
-    public float tempEfficiency;
-    public float wateringEfficiency;
+    public DependenceAttribute GetDepComp(GameObject dep)
+    {
+        return dep.GetComponent<DependenceAttribute>();
+    }
 
-    private float [] BalanceFloats(float [] values, float pivotIndex, float cutOff)
+    public float DepRateSum(GameObject [] values)
+    {
+        float sum = 0;
+        foreach (GameObject obj in values)
+            sum = sum + GetDepComp(obj).dependencyAmount;
+
+        return sum;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BalanceFloats(allDependencies, 0, maxEfficiency);
+    }
+
+
+    private void BalanceFloats(GameObject [] values, int pivotIndex, float cutOff)
     {
         //balances array so all the values add up to the cutOff
         //pivot is the index that the other values get balanced around
 
-        float decValue = values.Sum() - cutOff;
+        float decValue = DepRateSum(values) - cutOff;
 
         if (decValue > 0)
         {
@@ -41,7 +52,7 @@ public class PlantRates : MonoBehaviour
             {
                 if (i != pivotIndex)
                 {
-                    if (maxIndex == -1 || values[i] > values[maxIndex])
+                    if (maxIndex == -1 || GetDepComp(values[i]).dependencyAmount > GetDepComp(values[maxIndex]).dependencyAmount)
                         maxIndex = i;
                 }
             }
@@ -52,88 +63,53 @@ public class PlantRates : MonoBehaviour
                 {
                     if (i != pivotIndex)
                     {
-                        if (values[i] == values[maxIndex])
-                            values[i] = values[i] - decValue;
+                        if (GetDepComp(values[i]).dependencyAmount == GetDepComp(values[maxIndex]).dependencyAmount)
+                            GetDepComp(values[i]).dependencyAmount = GetDepComp(values[i]).dependencyAmount - decValue;
                         else if (decValue < 1)
-                            values[i] = values[i] - (decValue/2);
+                            GetDepComp(values[i]).dependencyAmount = GetDepComp(values[i]).dependencyAmount - (decValue/2);
                     }
                 }
              
                 for (int i = 0; i < values.Length; i++)
-                    values[i] = NumOp.Cutoff(values[i], 0f, cutOff);
+                    GetDepComp(values[i]).dependencyAmount = NumOp.Cutoff(GetDepComp(values[i]).dependencyAmount, 0f, cutOff);
             }
         }
-
-        return values;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        allRates = new[] { lightDependanceRate, soilDependanceRate, tempDependanceRate, wateringDependanceRate };
-        BalanceFloats(allRates, allRates.Max(), maxEfficiency);
-    }
-
     
 
-    public float GetDepRate(int index)
+    public float GetCurrentEfficiency(GameObject[] values)
     {
-        if (index == 0)
-            return lightDependanceRate;
-        else if (index == 1)
-            return soilDependanceRate;
-        else if (index == 2)
-            return tempDependanceRate;
-        else if (index == 3)
-            return wateringDependanceRate;
-        else
-            return -1f;
-    }
-
-    public float GetCurrentEfficiency()
-    {
-        float lightRate = GetDepRate(0) * lightEfficiency;
-        float soilRate = GetDepRate(1) * soilEfficiency;
-        float tempRate = GetDepRate(2) * tempEfficiency;
-        float waterRate = GetDepRate(3) * wateringEfficiency;
-
-        float currEffic = lightRate + soilRate + tempRate + waterRate;
+        float currEffic = 0f;
+        string text = "";
+        foreach (GameObject obj in values)
+        {
+            currEffic = currEffic + (GetDepComp(obj).dependencyAmount * GetDepComp(obj).dependencyEfficiency);
+            text = text + String.Format("{0}: {1} ", obj.name, GetDepComp(obj).dependencyAmount * GetDepComp(obj).dependencyEfficiency);
+        }
         if (name == "Aloe")
-            print(String.Format("LightRate: {0}, SoilRate: {1}, tempRate: {2}, waterRate: {3}, Total Efficiency: {4}", 
-                                                                                                                    lightRate, 
-                                                                                                                    soilRate, 
-                                                                                                                    tempRate, 
-                                                                                                                    waterRate, 
-                                                                                                                    currEffic));
+            print(text + " Current Efficiency: " + currEffic);
         return currEffic;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        lightEfficiency = NumOp.Cutoff(lightEfficiency, 0f, 1f);
-        soilEfficiency = NumOp.Cutoff(soilEfficiency, 0f, 1f);
-        tempEfficiency = NumOp.Cutoff(tempEfficiency, 0f, 1f);
-        wateringEfficiency = NumOp.Cutoff(wateringEfficiency, 0f, 1f);
 
         //get changed dependancy and balance
-        float changedIndex = -1;
-        for (int i=0; i<allRates.Length; i++)
+        int changedIndex = -1;
+
+        for (int i=0; i < allDependencies.Length; i++)
         {
-            if (allRates[i] != GetDepRate(i))
+            if (GetDepComp(allDependencies[i]).DepAmountChanged())
                 changedIndex = i;
-            allRates[i] = GetDepRate(i);
         }
 
-        if (changedIndex != -1f)
+        if (changedIndex != -1)
         {
-            BalanceFloats(allRates, changedIndex, maxEfficiency);
-            lightDependanceRate = allRates[0];
-            soilDependanceRate = allRates[1];
-            tempDependanceRate = allRates[2];
-            wateringDependanceRate = allRates[3];
+            BalanceFloats(allDependencies, changedIndex, maxEfficiency);
         }
-        float currEffic = GetCurrentEfficiency();
+        float currEffic = GetCurrentEfficiency(allDependencies);
 
         currentSecondsTillDeath = (int)((float)SecondsTillDeath * currEffic);
         if (name == "Aloe")
