cd72fecf203ca4158b1f7897d302a24c95dd2f05
erika-r <430erika@gmail.com>
2021-01-28 13:30:58 +0000

    change instantiated plants' parent to content

--- Assets/scripts/DisplayPlants.cs
+++ Assets/scripts/DisplayPlants.cs
@@ -23,8 +23,9 @@ public class DisplayPlants : MonoBehaviour
         for (int i = 0; i < numberToCreate; i++)
         {
             //creates and positions prefabs
-            newObj = (GameObject)Instantiate(prefab, new Vector3((i * 20.0F) + 10 , -100, -35), Quaternion.identity);
-            
+            newObj = (GameObject)Instantiate(prefab, new Vector3((i * 20.0F) + 10 , -100, -20), Quaternion.identity);
+            newObj.GetComponent<Transform>().SetParent(GetComponent<Transform>());
+
             //trying to fix issue with aloe not displaying correctly with grid layout component
             // newObj.transform.position = new Vector3(newObj.transform.position.x,newObj.transform.position.y, 0);
             // DontDestroyOnLoad(newObj);
