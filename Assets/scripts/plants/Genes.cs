72ddb55e5acd96da907cfbe971ed8b2a819f4193
Daniel Irein <daniel.irein2@mail.dcu.ie>
2021-01-31 18:21:33 +0000

    fixed gene script

--- Assets/scripts/plants/Genes.cs
+++ Assets/scripts/plants/Genes.cs
@@ -16,7 +16,6 @@ public class Genes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(genesMappings[0].name);
     }
 
     // Update is called once per frame
