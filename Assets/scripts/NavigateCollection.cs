9e74e23a84100881bb8ca4a009626cc377d92490
erika-r <430erika@gmail.com>
2021-02-02 20:25:26 +0000

    handled index error

--- Assets/scripts/NavigateCollection.cs
+++ Assets/scripts/NavigateCollection.cs
@@ -22,23 +22,24 @@ public class NavigateCollection : MonoBehaviour
         plantList = plantManager.plantCollection;
         indexNum = 0;   //starts at 0
 
-
         //when instantiated, the object is not a child of any object or any canvas so no images will appear on screen
-        //Setting the parent to Plant Display object
-        var parent = GameObject.Find("Plant Display").transform;
-        
+        //Setting the parent to Plant Display object        
         // display first plant in the list
         for (int i = 0; i <plantList.Count; i++) {
             //instantiate object - set position, rotation, and parent
             GameObject plant = Instantiate(plantList[i], new Vector3(70,-100,-20),Quaternion.identity, parent);
 
-            if (i != 0) {
-                plant.SetActive(false);     //make them inactive so they do not show on collection
-            }
+            plant.SetActive(false);     //make them inactive so they do not show on collection
         }
+        display();
+    }
+
+    void display(){
+
+        plantList[indexNum].SetActive(true);    //make plant visible
 
         //change name to current plant
-        nameText.text = "Name: " + plantList[0].name;
+        nameText.text = "Name: " + plantList[indexNum].name;
 
         //set index amount
         indexText.text = (indexNum + 1).ToString() + "/" + plantList.Count.ToString();
@@ -54,12 +55,13 @@ public class NavigateCollection : MonoBehaviour
 
         if (rightButton.GetComponent<NavigateButtons>().clicked == true) {
             navigate(true,rightButton);
-        } 
+        }
     }
 
     //displays and navigates between plants in list
     void navigate(bool next, Button button) {
 
+        try {
             //make current plant inactive
             plantList[indexNum].SetActive(false);
             
@@ -78,6 +80,22 @@ public class NavigateCollection : MonoBehaviour
 
             indexText.text = (indexNum + 1).ToString() + "/" + plantList.Count.ToString();
             button.GetComponent<NavigateButtons>().clicked = false;         //reset button status        
+
+        } catch (System.ArgumentOutOfRangeException e1){      //when index is out of range
+            if (indexNum == plantList.Count) {       //restarts
+                indexNum = 0;
+
+            } else if (indexNum <= 0) {
+                indexNum = plantList.Count - 1;
+            }
+
+            Debug.Log("Exception caught: " + e1);
+            button.GetComponent<NavigateButtons>().clicked = false;         //reset button status
+            display();
+            
+        }     
+        // finally {}      //do nothing
+
     }
 
 }
