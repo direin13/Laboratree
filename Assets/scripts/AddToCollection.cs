6484f85438aeba321d43c3935f378ec364d87127
erika-r <430erika@gmail.com>
2021-02-16 16:29:01 +0000

    updated spawn page
    added code to make program adding to collection more robust - same name cannot be added twice, no name cannot be used, added confirmation of success/failure popup

--- Assets/scripts/AddToCollection.cs
+++ Assets/scripts/AddToCollection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,13 +26,21 @@ public class AddToCollection : MonoBehaviour
         var text = NameInput.text;
 
         if (text.Length == 0) {
+            GameManager.GetComponent<PopUpManager>().PopUpMessage("No name entered!");
         } else {
-        GameManager.GetComponent<PlantManager>().MakePlant(NameInput.text, plantPrefab.name);
+            try {
+                GameManager.GetComponent<PlantManager>().MakePlant(NameInput.text, plantPrefab.name);
+                GameManager.GetComponent<PopUpManager>().PopUpMessage(String.Format("'{0}' has been added to the collection", NameInput.text));
+            } catch (ArgumentException e) {
+                GameManager.GetComponent<PopUpManager>().PopUpMessage(String.Format("'{0}' is already in the collection", NameInput.text));
+                Debug.Log("Exception caught: " + e);
+            }
 
         }
     }
 
     public void cancelAdd(){
+        NameInput.text="";
         panel.SetActive(false);
     }
 
